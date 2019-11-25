fcb4cf226a12a3b07c74bf71a9230870c7bc7e47
seekelvis <358191817@qq.com>
2019-11-25 21:34:53 +0000

    Update SQS_receive.py

--- SQS_receive.py
+++ SQS_receive.py
@@ -28,14 +28,17 @@ for receive in range(1, 4, 1):
     message = content["Messages"][0]
     receipt_handle = message["ReceiptHandle"]
     f1.write("run2" + "\n")
+    taskUnit = int(message['Body'])
     sqs.delete_message(
         QueueUrl=queue_url,
         ReceiptHandle=receipt_handle
     )
 
+    
+
 
     print("Received and deleted message: %s" % message)
 
-    f.write("Received and deleted message: "+str(message)+"\n")
+    f.write("Received and deleted message: "+str(taskUnit)+"\n")
 f.close()
 f1.close()
